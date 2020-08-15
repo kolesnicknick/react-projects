39744809019a4ee352f4761d806bfd1d11e21379
kolesnicknick <kolesniknikolai92@gmail.com>
2020-08-15 15:41:34 +0300

    heroku upload

--- contact-keeper/src/server.ts
+++ contact-keeper/src/server.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import Router from './routes/index';
 import { Express } from 'express-serve-static-core';
 import * as db from './config/db';
+import { response } from 'express';
+import path from 'path';
 
 
 const app: Express = express();
@@ -13,6 +15,14 @@ app.use(express.json({ extended: false }));
 const add = (a: number, b: number): number => a + b;
 
 Router.configure(app);
+
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+        return response.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
